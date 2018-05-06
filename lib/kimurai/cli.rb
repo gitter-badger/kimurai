0805caf74738aaf03947583b64034e28c5ff01f9
Victor Afanasev <vicfreefly@gmail.com>
2018-05-06 18:37:23 +0400

    Add .open_crawler and .close_crawler for pipelines

--- lib/kimurai/cli.rb
+++ lib/kimurai/cli.rb
@@ -7,6 +7,14 @@ module Kimurai
       require './config/boot'
 
       crawler_class = Base.descendants.find { |crawler| crawler.name == crawler_name }
+
+      # call .open_crawler method for each crawler's pipeline
+      pipelines = crawler_class.pipelines.map { |pipeline| pipeline.to_s.classify.constantize }
+      pipelines.each { |pipeline| pipeline.try(:open_crawler) }
+
+      # define at_exit proc with calling .close_crawler method for each crawler's pipeline
+      at_exit { pipelines.each { |pipeline| pipeline.try(:close_crawler) }}
+
       crawler_class.new.parse
     end
 

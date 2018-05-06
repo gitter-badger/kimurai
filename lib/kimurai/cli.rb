96b44881d8bf62acae3891776c2e9e349c22e077
Victor Afanasev <vicfreefly@gmail.com>
2018-05-06 17:19:09 +0400

    Add Cli#list

--- lib/kimurai/cli.rb
+++ lib/kimurai/cli.rb
@@ -9,5 +9,15 @@ module Kimurai
       crawler_class = Base.descendants.find { |crawler| crawler.name == crawler_name }
       crawler_class.new.parse
     end
+
+    desc "list", "List all crawlers in the project"
+    def list
+      require './crawlers/application_crawler'
+      require_all "crawlers"
+
+      Base.descendants.each do |crawler|
+        puts crawler.name if crawler&.name
+      end
+    end
   end
 end
